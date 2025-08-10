1a677d13e39eab0da1ee058b74338e8da59d85b3
Salomon Uwimana Masasu <113304910+Masasu74@users.noreply.github.com>
2025-08-10 20:51:07 +0200

    A

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
+import TrendingCard from "@/components/TrendingCard";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
@@ -73,7 +74,10 @@ export default function Index() {
                 data={trendingMovies}
                 renderItem={({ item, index }) =>(
                   
-                  <Text className="text-white text-sm">{item.title}</Text>
+                  <TrendingCard 
+                  movie={item}
+                  index={index}
+                  />
         )}
         keyExtractor={(item)=>item.movie_id.toString()}
               />
